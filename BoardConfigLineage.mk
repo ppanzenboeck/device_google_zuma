20f101ac26b9a5af13b97f001953a8f548439fd7
NurKeinNeid <mralexman3000@gmail.com>
2024-11-23 16:20:38 +0100

    zuma: Set DISABLE_ARTIFACT_PATH_REQUIREMENTS to true
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
