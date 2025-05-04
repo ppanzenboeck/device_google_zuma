ca5790ef39a9d71665e8ec27aa9aba650f39234e
adi8900 <adrianszymanski242@gmail.com>
2025-05-04 20:33:06 +0200

    zuma: Drop duplicated prop
    * it is being set in vendor/lineage in derpfest
    
    Change-Id: I83b6fc14f11c5c80758ea894a5216f7057d7a489
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -23,9 +23,6 @@ PRODUCT_PACKAGES += \
 # Experiments
 include device/google/gs-common/performance/experiments/experiments.mk
 
-# Google Assistant
-PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
-
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
