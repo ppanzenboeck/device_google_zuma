dfe65813bd5cd6126c0241ee01c40cf5618a3be4
ppanzenboeck <panz.development@gmail.com>
2025-07-03 20:30:49 +0200

    zuma: Exclude some apps from builds

--- lineage_common.mk
+++ lineage_common.mk
@@ -40,6 +40,10 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringOverlay
